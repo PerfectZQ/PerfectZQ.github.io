d6c6a64a98e31ea63ab5f6952f1ee655239f66b6
zhangqiang <inteli_zq@163.com>
2017-10-13 15:24:47 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 $(document).ready(function() {
-
-
-  $('a.blog-button').click(function() {
-    if (currentWidth < 2000) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '320px', 'width': '22%'}, 400, swing = 'swing', function() {} );
-    }
-  });
-
-  if (window.location.pathname.substring(0, 5) == "/tag/") {
+  /*if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
-  }
+  }*/
 });
