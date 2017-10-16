4b3ba9d97cd67a5945a9e67d24a75082beb7f872
zhangqiang <inteli_zq@163.com>
2017-10-16 17:53:28 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ $(function(){
      * 侧页一级目录点击事件
      */
     $("li.first_content").click(function(){
-       var ul = $(this).next();
-       alert(ul.html());
+       var ul = $(this).children('ul');
        var className = ul.attr('class');
        if(className == 'disappear'){
            ul.removeClass('disappear');
