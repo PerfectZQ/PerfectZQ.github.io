5bb6fb51225ac60e66880bd1395c38b64c69bb0a
PerfectZQ <inteli_zq@163.com>
2017-10-17 23:15:04 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -1,19 +1,9 @@
 $(function () {
 
-    $(".sidebar-container").niceScroll(".menu",
-        {
-            railoffset: top,
-            nativeparentscrolling: true
-        }
-    );
-    $("body").niceScroll();
-
     /**
      * 侧页一级目录点击事件
      */
     $("li.first_content").click(function () {
-        $('.sidebar-container').getNiceScroll().show();
-        $('.sidebar-container').getNiceScroll().resize();
         var ul = $(this).children('ul');
         var className = ul.attr('class');
         if (className == 'disappear') {
