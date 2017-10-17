26dee307313de4522d3096a71092fa9d57954662
zhangqiang <inteli_zq@163.com>
2017-10-17 17:52:15 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-$(function(){
+$(function () {
     /**
      * 侧页一级目录点击事件
      */
-    $("li.first_content").click(function(){
-       var ul = $(this).children('ul');
-       var className = ul.attr('class');
-       if(className == 'disappear'){
-           ul.removeClass('disappear');
-           ul.addClass('appear');
-       }else{
-           ul.removeClass('appear');
-           ul.addClass('disappear');
-       }
+    $("li.first_content").click(function () {
+        var ul = $(this).children('ul');
+        var className = ul.attr('class');
+        if (className == 'disappear') {
+            ul.removeClass('disappear');
+            ul.addClass('appear');
+        } else {
+            ul.removeClass('appear');
+            ul.addClass('disappear');
+        }
     });
 
 
     $(".sidebar-container").niceScroll();
+    $("body").niceScroll();
 });
 
