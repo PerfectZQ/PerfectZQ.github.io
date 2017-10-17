fa3c060e04fdfee245873df9f8091803568be394
PerfectZQ <inteli_zq@163.com>
2017-10-17 22:43:33 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(function () {
 
-    $(".sidebar-container").niceScroll();
+    $(".sidebar-container").niceScroll(".menu");
     $("body").niceScroll();
 
     /**
      * 侧页一级目录点击事件
      */
     $("li.first_content").click(function () {
-        $(".sidebar-container").getNiceScroll().show();
-        $(".sidebar-container").getNiceScroll().resize();
+        $(".sidebar-container").getNiceScroll(".menu").show();
+        $(".sidebar-container").getNiceScroll(".menu").resize();
         var ul = $(this).children('ul');
         var className = ul.attr('class');
         if (className == 'disappear') {
