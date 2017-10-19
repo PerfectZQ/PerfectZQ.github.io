eb43d38f17b904a0ba4c04e3dcfc4857f6ee77f7
PerfectZQ <inteli_zq@163.com>
2017-10-19 22:20:23 +0800

    modify

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 $(function () {
 
     $("body").niceScroll({
-        cursorcolor: "#B9D3EE"
+        cursorcolor: "#B9D3EE",
+        enablescrollonselection: true
     });
 
     $("pre").niceScroll({
         cursorcolor: "#B9D3EE",
         // 当只有水平滚动时可以用鼠标滚轮来滚动，如果设为false则不支持水平滚动，如果设为auto支持双轴滚动
-        oneaxismousemode: "auto"
+        oneaxismousemode: "auto",
+        enablescrollonselection: true
     });
 
     /**
