3da430121b7c2daeb91512f77c14aaa5e3b16480
PerfectZQ <inteli_zq@163.com>
2017-10-19 23:06:23 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-$(document).ready(function() {
-  /*if (window.location.pathname.substring(0, 5) == "/tag/") {
-    $('.panel-cover').addClass('panel-cover--collapsed');
-  }*/
+$(function () {
+
+    // $("body").niceScroll({
+    //     cursorcolor: "#B9D3EE",
+    //     enablescrollonselection: true,
+    //     nativeparentscrolling: true
+    // });
+    //
+    // $("pre").niceScroll({
+    //     cursorcolor: "#B9D3EE",
+    //     // 当只有水平滚动时可以用鼠标滚轮来滚动，如果设为false则不支持水平滚动，如果设为auto支持双轴滚动
+    //     oneaxismousemode: "auto",
+    //     enablescrollonselection: true,
+    //     nativeparentscrolling: true
+    // });
+
+    /**
+     * 侧页一级目录点击事件
+     */
+    $("li.first_content").click(function () {
+        var ul = $(this).children('ul');
+        ul.slideToggle('normal');
+    });
+
+    /**
+     * header 头像点击事件
+     */
+    $("div.left-avatar").click(function () {
+        $("#dropdown-menu").slideToggle('normal');
+    });
+
 });
