4e5d4889970644e9033c45e3c8475c01e6c3fb43
zhangqiang <inteli_zq@163.com>
2017-12-20 14:38:17 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ $(function () {
     });
 
     $("a.blog-button").click(function () {
-        if (location.hash && location.hash == "#blog") {
-            var welcome_index = $(".welcome_index");
-            var content_inde = $(".content_index");
-            if (welcome_index.hasClass("disappear")) return;
-            welcome_index.addClass("disappear");
-            content_inde.removeClass("disappear");
-        }
+        // 如果目前已经在 #blog 直接返回
+        if (location.hash && location.hash == "#blog") return;
+        var welcome_index = $(".welcome_index");
+        var content_index = $(".content_index");
+        if (welcome_index.hasClass("disappear")) return;
+        welcome_index.addClass("disappear");
+        content_index.removeClass("disappear");
     });
 
 });
