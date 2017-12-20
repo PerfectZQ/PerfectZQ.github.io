8a5b526fe20105f6dc8cb61ddc511dad1e0ad94e
zhangqiang <inteli_zq@163.com>
2017-12-20 14:34:38 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ $(function () {
     });
 
     $("a.blog-button").click(function () {
-        if (location.hash && location.hash == "#blog") return;
-        var welcome_index = $(".welcome_index");
-        var content_inde = $(".content_index");
-        if (welcome_index.hasClass("disappear")) return;
-        welcome_index.addClass("disappear");
-        content_inde.removeClass("disappear");
-
+        if (location.hash && location.hash == "#blog") {
+            var welcome_index = $(".welcome_index");
+            var content_inde = $(".content_index");
+            if (welcome_index.hasClass("disappear")) return;
+            welcome_index.addClass("disappear");
+            content_inde.removeClass("disappear");
+        }
     });
 
 });
