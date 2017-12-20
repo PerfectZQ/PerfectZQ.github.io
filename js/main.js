4eb7cf991e22f7ed9f94ddefa00b8388a5a957fb
zhangqiang <inteli_zq@163.com>
2017-12-20 14:55:13 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -24,16 +24,17 @@ $(function () {
         content_index.removeClass("disappear");
     }
 
-    // $("a.blog-button").click(function () {
-    //     // 如果目前已经在 #blog 直接返回
-    //     if (location.hash && location.hash == "#blog") {
-    //         var welcome_index = $(".welcome_index");
-    //         var content_index = $(".content_index");
-    //         if (welcome_index.hasClass("disappear")) return;
-    //         welcome_index.addClass("disappear");
-    //         content_index.removeClass("disappear");
-    //     }
-    // });
+    $("a.blog-button").click(function () {
+        // 如果目前已经在 #blog 直接返回
+        // if (location.hash && location.hash == "#blog") {
+        //     var welcome_index = $(".welcome_index");
+        //     var content_index = $(".content_index");
+        //     if (welcome_index.hasClass("disappear")) return;
+        //     welcome_index.addClass("disappear");
+        //     content_index.removeClass("disappear");
+        // }
+        window.location.href="http://arch-long.cn/#blog";
+    });
 
 });
 
