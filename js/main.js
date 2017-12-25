846a8026e7957ed493620592a952eeaf6a5adabc
zhangqiang <inteli_zq@163.com>
2017-12-25 15:11:55 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -70,9 +70,6 @@ $(function () {
                     "   </a>" +
                     "</li>"
             }
-            if (html != "") {
-                alert(html)
-            }
 
             $(".sidebar_right ol").html(
                 $(".sidebar_right ol").html() +
