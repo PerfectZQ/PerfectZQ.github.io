0b16229204717d7e43be64508020646e72d7a67d
zhangqiang <inteli_zq@163.com>
2019-03-20 19:34:50 +0800

    modify

--- js/main.js
+++ js/main.js
@@ -222,7 +222,8 @@ function fontLoad() {
     }
     //判断是否为Chrome浏览器
     else if (explorer.indexOf("Chrome") >= 0) {
-        // $("html").css({"font-family": 'Microsoft YaHei, Consolas, Courier'});
+        $("html").css({"font-family": 'Microsoft YaHei, Consolas, Courier'});
+        $("a").css({"font-family": 'Microsoft YaHei, Consolas, Courier'});
     }
     //判断是否为Opera浏览器
     else if (explorer.indexOf("Opera") >= 0) {
